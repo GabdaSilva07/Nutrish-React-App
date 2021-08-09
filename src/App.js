74ad3283b07758874322704b019aff63d93c85d8
Gabriel da Silva <gabriel.dasilva2017@my.ntu.ac.uk>
2021-08-09 18:30:14 +0100

    Added Recipe pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Components/LoginPage";
 import Home from "./Components/Home"
 import Separator from "./Components/Separator";
+import Registration from "./Components/Registration";
+import RecipeDetail from "./Components/RecipeDetail";
 
 function App() {
   return (
@@ -13,10 +15,11 @@ function App() {
       <div className="App">
         <Nav />
         <Separator/>
-        
         <Switch>
-          <Route path = "/" exact component = {Home}/>
+          <Route exact path = "/" component = {Home}/>
           <Route path = "/login" component={Login}/>
+          <Route path="/registration" component={Registration}/>
+          <Route path="/recipe/:id" component={RecipeDetail}/>
         </Switch>
       </div>
     </Router>
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "./CSS/Row.css";
+import { Link } from "react-router-dom";
 
 const base_url = "https://spoonacular.com/recipeImages/";
 const image_size = "312x231";
-const image_type ="jpg"
+const image_type = "jpg";
 function Row({ title, fetchUrl, isLargeRow }) {
+
+  const linkStyle = {
+      display: "flex",
+      padding: "5px"
+  }
   const [cuisines, setCuisines] = useState([]);
 
   useEffect(() => {
@@ -17,22 +23,21 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
-
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="rows_poster">
         {cuisines.map((dish) => (
-          <img
-            key={dish.id}
-            className={`row_poster`}
-            src={`${base_url}${dish.id}-${image_size}.${image_type}`}
-            alt={dish.title}
-          />
+          <Link style={linkStyle} to={`/recipe/${dish.id}`} key={dish.id}>
+            <img
+              key={dish.id}
+              className={`row_poster`}
+              src={`${base_url}${dish.id}-${image_size}.${image_type}`}
+              alt={dish.title}
+            />
+          </Link>
         ))}
       </div>
-
-      {/* Image*/}
     </div>
   );
 }
