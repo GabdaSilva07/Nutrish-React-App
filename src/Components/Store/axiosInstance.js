5752f718823e2068354bd596770deb55f589b517
GabdaSilva07 <gabdasilva96@gmail.com>
2021-12-27 13:36:55 +0000

    App is working remotely

--- src/Components/Store/axiosInstance.js
+++ src/Components/Store/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://nutrish-django-ntcdz.ondigitalocean.app/";
+const baseURL = "http://127.0.0.1:8000/";
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
